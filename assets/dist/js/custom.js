bfc2fbe7dcac782699074131eb0785eaf7f53760
webappsettings <webappsettings@gmail.com>
2022-10-18 02:42:06 +0530

    updated

--- assets/dist/js/custom.js
+++ assets/dist/js/custom.js
@@ -73,6 +73,101 @@
        
 //----mycode end
 
+
+
+var swalathAddFormValidator = $("#swalathAddForm").validate({
+      ignore: "",
+      errorElement: 'span',
+      errorClass: 'error text-danger',
+      errorPlacement: function (error, element) {
+      if (element.hasClass("btn-check")) {
+          error.appendTo(element.parent().parent().parent());
+      } else {
+          error.appendTo(element.parent());
+      }
+      },
+      rules: {
+          nameInput: {
+            required: true,
+          },
+          phoneInput: {
+            required: true,
+            number: true,
+          },
+          swalathInput: {
+            required: true,
+            number: true,
+          },
+      },
+      messages: {
+          nameInput: {
+              required: "നിങ്ങളുടെ പേര് ചേര്‍ക്കുക",
+          },
+          phoneInput: {
+              required: "നിങ്ങളുടെ ഫോണ്‍ നമ്പര്‍ ചേര്‍ക്കുക",
+              number: "നിങ്ങളുടെ ഫോണ്‍ നമ്പര്‍ ചേര്‍ക്കുക",
+          },
+          swalathInput: {
+              required: "നിങ്ങള്‍ ചൊല്ലിയ സ്വലാത്തുകളുടെ എണ്ണം ചേര്‍ക്കുക",
+              number: "നിങ്ങള്‍ ചൊല്ലിയ സ്വലാത്തുകളുടെ എണ്ണം ചേര്‍ക്കുക",
+          },
+          
+      },
+      submitHandler: function () {
+          
+      },
+  
+});
+
+$('#save-swalath-btn').on('click', function() {
+    if($("#swalathAddForm").valid()) {
+      var getName = $('#nameInput').val()
+          var getPhone = $('#phoneInput').val()
+          var getSwalath = $('#swalathInput').val()
+
+          let formdata = new FormData()
+          formdata.append('action', 'vw')
+          formdata.append('name', getName)
+          formdata.append('phone', getPhone)
+          formdata.append('swalath', getSwalath)
+          formdata.append('bowser', browserDetect)
+          formdata.append('systemcode', systemcode)
+
+          $.ajax({
+             method: 'POST',
+             url: gcode,
+             data: formdata,
+             dataType: 'json',
+             contentType: false,
+             processData: false,
+             beforeSend: function(){
+              console.log('before send')
+                // $('.loader').fadeIn()
+              }
+            })
+            .done(function(callback){
+              // callback = JSON.parse(callback.result)
+              $('#totalSwalathView').removeClass('d-none');
+
+              console.log('result= ',callback)
+              $('#yourTotalSwalathView span').text(callback.result.YourSwalath);
+              $('#totalSwalathView span').text(callback.result.TotalSwalath);
+              $('#swalathInput').val('')
+              /*if(!callback.result) {
+              }*/ 
+
+            })
+            .fail(function(callback) {
+            })
+           .always(function(){
+              // $('.loader').fadeOut()
+              // console.log('after send')
+            });
+    }
+});
+
+var gcode = 'https://script.google.com/macros/s/AKfycbwwyBlpCOGIer2oUuROwNKpcPYSFsTazW-mrm6Wj-CnTBDe6zX6cj2dNAayJe1asMVL/exec';
+
     var xtraDetails = Object.keys(bowser).filter(function (key) {
         if (bowser[key] === true) {
           return bowser[key];
@@ -81,7 +176,7 @@
 
       var xtraDetails = JSON.stringify(xtraDetails).replace(/[{}]/g, "").replace(/,/g, "  ").replace(/\"/g, "");
       var browserDetect = bowser.name + "-" + bowser.version + "  " + bowser.osname + (bowser.osversion ? "-" + bowser.osversion : '') + " " + xtraDetails;
-      console.log('browserDetect=', browserDetect);
+      // console.log('browserDetect=', browserDetect);
 
       var systemcode
 
@@ -90,53 +185,40 @@
 
         systemcode = result
 
-        console.log(result)
+
+
+        var formdata = new FormData()
+          formdata.append('action', 'chk')
+          formdata.append('systemcode', systemcode)
+
+          $.ajax({
+             method: 'POST',
+             url: gcode,
+             data: formdata,
+             dataType: 'json',
+             contentType: false,
+             processData: false,
+             beforeSend: function(){
+              }
+            })
+            .done(function(callback){
+              // console.log(callback)
+              if(callback.result == "false") {
+
+              } else {
+                $('#nameInput').val(callback.result.Name).prop('disabled', false);
+                $('#phoneInput').val(callback.result.Phone).prop('disabled', false);
+                $('#swalathInput').prop('disabled', false);
+                $('#yourTotalSwalathView span').text(callback.result.Swalath);
+              }
+              $('#save-swalath-btn').prop('disabled', false);
+            })
+            .fail(function(callback) {
+            })
+           .always(function(){
+             
+            });
       });
 
-    $('#save-swalath-btn').on('click', function() {
 
-
-      var getName = $('#nameInput').val()
-      var getPhone = $('#phoneInput').val()
-      var getSwalath = $('#swalathInput').val()
-
-      let formdata = new FormData()
-      formdata.append('action', 'vw')
-      formdata.append('name', getName)
-      formdata.append('phone', getPhone)
-      formdata.append('swalath', getSwalath)
-      formdata.append('bowser', browserDetect)
-      formdata.append('systemcode', systemcode)
-
-      $.ajax({
-         method: 'POST',
-         url: 'https://script.google.com/macros/s/AKfycbyOdRoM_PEBhULAFUyA6XlwmGWcnqiRmwyn_B26FrtlxDwYGu5L-4O3iCUI0t936c8y/exec',
-         data: formdata,
-         dataType: 'json',
-         contentType: false,
-         processData: false,
-         beforeSend: function(){
-          console.log('before send')
-            // $('.loader').fadeIn()
-          }
-        })
-        .done(function(callback){
-          // callback = JSON.parse(callback.result)
-
-          $('#totalSwalathView span').html(callback.result)
-          $('#swalathInput').val('')
-
-          console.log('result= ',callback)
-          /*if(!callback.result) {
-          
-
-          }*/ 
-
-        })
-        .fail(function(callback) {
-        })
-       .always(function(){
-          // $('.loader').fadeOut()
-          console.log('after send')
-        });
-  })
+      
